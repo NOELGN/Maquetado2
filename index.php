ac92f08c69a1788e6b8fac3ea42d789518b0f228
NOELGN <noelgn@outlook.com>
2019-09-14 23:22:58 -0500

    primer version

---
+++ index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>titulo de la pagina</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <header id="encabezado">
+        <section id="banner">
+            <h1>banner</h1>
+        </section>
+
+        <div id="divtitulo">
+
+        <section id="titulo">
+            <h1>titulo</h1>
+        </section>
+
+        <section id="subtitulo">
+            <h1>subtitulo</h1>
+        </section>
+
+        </div>
+
+        <section id="buscador">
+            <h2>buscador</h2>
+        </section>
+
+        <nav>
+            <h1>nav</h1>
+        </nav>
+    </header>
+
+    <main>
+        <div id="contenido1">
+            <article id="articulo1">
+                <h2>articulo1</h2>
+            </article>
+            <section id="con1">
+
+                <article id="articulo2">
+                    <h2>articulo2</h2>
+                </article>
+                <article id="articulo2">
+                    <h2>articulo3</h2>
+                </article>
+                <article id="articulo2">
+                    <h2>articulo4</h2>
+                </article>
+            </section>
+
+            <section id="con2">
+                <article id="articulo3">
+                    <h2>articulo5</h2>
+                </article>
+            </section>
+
+            <section id="con3">
+                <article id="articulo41">
+                    <h2>articulo6</h2>
+                </article>
+                <article id="articulo4">
+                    <h2>articulo7</h2>
+                </article>
+                <article id="articulo4">
+                    <h2>articulo8</h2>
+                </article>
+            </section>
+
+            <section id="con4">
+            <article id="articulo55">
+                    <h2>articulo11</h2>
+                </article>
+                <article id="articulo5">
+                    <h2>articulo9</h2>
+                </article>
+                <article id="articulo55">
+                    <h2>articulo10</h2>
+                </article>
+                
+            </section>
+
+        </div>
+
+        <div id="contenido2">
+            <h1>aside</h1>
+        </div>
+    </main>
+
+    <footer>
+        <h1>footer</h1>
+    </footer>
+
+    <section></section>
+</body>
+
+</html>
